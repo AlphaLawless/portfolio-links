75868cfa03073cef82f07b21432bb2446c6460b5
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2021-12-06 02:50:37 -0300

    update: added more focus in links with reduction of cursor animation.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,33 @@
+// Variables
+const section = document.querySelector(".container");
+
 // Animation hover
-VanillaTilt.init(document.querySelectorAll(".container"), {
+VanillaTilt.init(section, {
   max: 25,
   speed: 400,
 });
 
 // Cursor
+const divCursor = document.querySelector("div");
+const linkable = document.querySelectorAll("a");
 const cursor = document.querySelector(".cursor");
 
 document.addEventListener("mousemove", (event) => {
   cursor.setAttribute(
     "style",
-    `top: ${event.pageY + 2}px; left: ${event.pageX - 2}px; display: block;`
+    `top: ${event.pageY}px; left: ${event.pageX}px; display: block;`
   );
+});
+
+for (let link of linkable) {
+  link.addEventListener("mouseover", (event) => {
+    divCursor.classList.remove("cursor");
+    event.target.style.cursor = "pointer";
+  });
+}
+
+section.addEventListener("mouseleave", () => {
+  divCursor.classList.add("cursor");
 });
 
 document.addEventListener("click", () => {
